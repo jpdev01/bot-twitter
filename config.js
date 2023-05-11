fe417433ba12998f8e8eafd843a2eff182ec358e
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2023-05-11 09:19:04 -0300

    upd: remove arquivo

--- config.js
+++
@@ -1,7 +0,0 @@
-// Put your own Twitter App keys here. See README.md for more detail.
-module.exports = {
-    consumer_key:         'c6fMvqoDdRoXWJjUkCP3IRQSu',
-    consumer_secret:      'Mnb2jVeZyZswSd8cNy1CmWXqeKfGUfVLk0Rs9WQQo8DiyOLNG3',
-    access_token:         '1081621294255390721-QQCrCkbKCjV5bZrXjW0matPs4b1Pw9',
-    access_token_secret:  '9RAPbPlWpTGvEJT94F4Kwu80heSQbsw2KfgEG2gunjGzV'
-}
